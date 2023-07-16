8f07361088c341c3b585cbc563d0fe1bacf121a8
박지호 <yoho4613@gmail.com>
2023-07-16 12:10:01 +1200

    payment api done

---
+++ pages/api/payment/payment.ts
@@ -0,0 +1,51 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../db";
+import { z } from "zod";
+import { BASE_URL } from "../../constant/config";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    console.log(req.body);
+    const paymentSchema = z.object({
+      amount: z.number(),
+      method: z.string(),
+      transactionId: z.number(),
+    });
+
+    const response = paymentSchema.safeParse(JSON.parse(req.body));
+    if (!response.success) {
+      res.status(400).json("Invalid Payment Type");
+    }
+
+    const payment = await prisma.payment.create({
+      data: JSON.parse(req.body),
+    });
+
+    if (!payment) {
+      res.status(401).json("Could not process with payment");
+    }
+
+    const transaction = await prisma.transaction.update({
+      where: {
+        id: JSON.parse(req.body).transactionId,
+      },
+      data: {
+        paid: {
+          increment: payment.amount,
+        },
+      },
+    });
+
+    if(transaction.subtotal <= transaction.paid) {
+      await fetch(`${BASE_URL}/table/isParticipated`, {
+        method: "POST",
+        body: ""
+      })
+    }
+
+    res.status(200).json(transaction);
+  }
+}
--- pages/transaction/payment/[id].tsx
+++ pages/transaction/payment/[id].tsx
@@ -1,8 +1,9 @@
 import { GetServerSideProps } from "next";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BASE_URL } from "../../constant/config";
 import { Order, TransactionType } from "../../../config/type";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 interface PaymentProps {
   query: { id: string };
@@ -11,8 +12,35 @@ interface PaymentProps {
 }
 
 const Payment = ({ query, transaction, orders }: PaymentProps) => {
-  const [payment, setPayment] = useState<number>(transaction.subtotal)
-  console.log(transaction);
+  const router = useRouter();
+  const [payment, setPayment] = useState<number>(
+    transaction.subtotal - Number(transaction.paid)
+  );
+
+  useEffect(() => {
+    setPayment(transaction.subtotal - Number(transaction.paid));
+  }, [router]);
+
+  const handlePayment = async (method: string) => {
+    const form = {
+      method,
+      amount: Number(payment),
+      transactionId: Number(transaction.id),
+    };
+
+    const response = await fetch(`${BASE_URL}/api/payment/payment`, {
+      method: "POST",
+      body: JSON.stringify(form),
+    })
+      .then((res) => res.json())
+      .catch((err) => console.log(err));
+    if (response.subtotal <= response.paid) {
+      router.push("/");
+    } else {
+      router.replace(router.asPath);
+    }
+  };
+
   return (
     <div className="bg-[#002A53] w-screen min-h-screen flex flex-col justify-center items-center ">
       <div className="bg-gray-200 w-4/5 p-6 rounded-lg flex">
@@ -35,30 +63,41 @@ const Payment = ({ query, transaction, orders }: PaymentProps) => {
             </div>
           </div>
           <div className="flex justify-between border-blue-400 border-b-2 mb-2">
-            <h2>Upfront Payment:</h2>
-            <p>${transaction.paid}</p>
-          </div>
-          <div className="flex justify-between border-blue-400 border-b-2 mb-2">
-            <h2>Remaining Balance:</h2>
+            <h2>Subtotal Amount:</h2>
             <p>${transaction.subtotal}</p>
           </div>
 
+          <div className="flex justify-between border-blue-400 border-b-2 mb-2">
+            <h2>Upfront Payment:</h2>
+            <p>${transaction.paid}</p>
+          </div>
           <div className="flex justify-between border-blue-400 border-b-2">
+            <h2>Remaining Payment:</h2>
+            <input
+              type="number"
+              className="text-right"
+              value={payment}
+              onChange={(e) => setPayment(Number(e.target.value))}
+            />
+          </div>
+
+          <div className="flex justify-between border-blue-400 border-b-2 mb-2">
             <h2>Remaining Balance:</h2>
-            <input type="number" className="text-right" value={payment} onChange={(e) => setPayment(Number(e.target.value))} />
+            <p>${transaction.subtotal - Number(transaction.paid)}</p>
           </div>
 
           <div className="flex justify-between items-center w-full mt-10">
             <div className="text-center ">
               <button
                 type="button"
-                // onClick={handleSave}
+                onClick={() => handlePayment("card")}
                 className="text-white w-1/3 mr-4 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm  sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
                 Card
               </button>
               <button
                 type="button"
+                onClick={() => handlePayment("cash")}
                 className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
               >
                 Cash
