255745bd9164a1f1d0582f030f33fcd9ddfa8e9b
박지호 <yoho4613@gmail.com>
2023-07-17 01:46:40 +1200

    sales page added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,11 +4,12 @@ import React, { useState } from "react";
 
 const Navbar = () => {
   const [opened, setOpened] = useState<boolean>(true);
+  const [dropdown, setDropdown] = useState<boolean>(false);
   const router = useRouter();
   const logout = async () => {
     await fetch("/api/auth/logout", {
       method: "POST",
-    }).then(() => router.push('/login'))
+    }).then(() => router.push("/login"));
   };
 
   return (
@@ -77,11 +78,9 @@ const Navbar = () => {
               </Link>
             </li>
             <li>
-              <button
-                type="button"
+              <Link
+                href="/sales"
                 className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                aria-controls="dropdown-example"
-                data-collapse-toggle="dropdown-example"
               >
                 <svg
                   aria-hidden="true"
@@ -98,48 +97,10 @@ const Navbar = () => {
                 </svg>
                 {opened && (
                   <span className="flex-1 ml-3 text-left whitespace-nowrap">
-                    E-commerce
+                    Sales
                   </span>
                 )}
-                <svg
-                  className="w-6 h-6"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-              </button>
-              {/* <ul id="dropdown-example" className="hidden py-2 space-y-2">
-                <li>
-                  <a
-                    href="#"
-                    className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                  >
-                    Products
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                  >
-                    Billing
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                  >
-                    Invoice
-                  </a>
-                </li>
-              </ul> */}
+              </Link>
             </li>
             <li>
               <Link
@@ -257,9 +218,7 @@ const Navbar = () => {
                   ></path>
                 </svg>
                 {opened && (
-                  <span className="flex-1 ml-3 whitespace-nowrap">
-                    Menu
-                  </span>
+                  <span className="flex-1 ml-3 whitespace-nowrap">Menu</span>
                 )}
               </Link>
             </li>
